e50abd3ca621113fa91b4441534b98a8a224267d
Aleksejs Uljanovs <aleksejs.uljanovs@csb.gov.lv>
2017-06-21 13:33:56 +0300

    Window Buttons fixed

--- View/Controls/WindowButtons.xaml.cs
+++ View/Controls/WindowButtons.xaml.cs
@@ -10,6 +10,20 @@ namespace zWPFStartupProject.View.Controls {
 		public WindowButtons() {
 			InitializeComponent();
 		}
-        
-	}
+
+        Window window { get { return Window.GetWindow(this); } }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+            SystemCommands.CloseWindow(window);
+        }
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e) {
+            SystemCommands.MaximizeWindow(window);
+        }
+        private void RestoreButton_Click(object sender, RoutedEventArgs e) {
+            SystemCommands.RestoreWindow(window);
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) {
+            SystemCommands.MinimizeWindow(window);
+        }
+    }
 }
